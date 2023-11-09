003ac78e2a6f7c13b6b234604879d421567c7b95
19khaled88 <kkhaled88hasan@gmail.com>
2023-11-09 12:26:29 +0600

    added cors option

--- src/app.ts
+++ src/app.ts
@@ -5,16 +5,42 @@ import globalErrorHandler from './app/middleware/globalErrorHandler'
 import userRoutes from './app/routes/index'
 
 const app: Application = express()
-
+const corsOptions = {
+  origin: '*',
+  credentials: true,
+  optionSuccessStatus: 200,
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+}
 
 
 //middlwares
-app.use(cors())
+app.use(cors(corsOptions))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
+// app.use((req, res, next) => {
+//   res.setHeader(
+//     "Access-Control-Allow-Origin",
+//     "https://your-frontend.com"
+//   );
+//   res.setHeader(
+//     "Access-Control-Allow-Methods",
+//     "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE"
+//   );
+//   res.setHeader(
+//     "Access-Control-Allow-Headers",
+//     "Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers"
+//   );
+//   res.setHeader("Access-Control-Allow-Credentials", true);
+//   res.setHeader("Access-Control-Allow-Private-Network", true);
+//   //  Firefox caps this at 24 hours (86400 seconds). Chromium (starting in v76) caps at 2 hours (7200 seconds). The default value is 5 seconds.
+//   res.setHeader("Access-Control-Max-Age", 7200);
+
+//   next();
+// });
 
 //routes
+
 app.use('/api/v1', userRoutes)
 
 app.get('/', (req: Request, res: Response) => {
